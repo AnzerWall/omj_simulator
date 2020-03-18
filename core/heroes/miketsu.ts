453f87388b432ebbf15b37aa2664dc656558af3d
anzerwall <anzerwall@gmail.com>
2020-03-18 23:16:51 +0800

    fix：修复御馔津属性错误

--- core/heroes/miketsu.ts
+++ core/heroes/miketsu.ts
@@ -56,15 +56,15 @@ const skill3: Skill = {
 export default class Miketsu extends Entity {
     constructor() {
         super();
-        this.setProperty(BattleProperties.MAX_HP, 3001.6);
-        this.setProperty(BattleProperties.ATK, 12646.12);
+        this.setProperty(BattleProperties.MAX_HP, 12646.12);
+        this.setProperty(BattleProperties.ATK, 3001.6);
         this.setProperty(BattleProperties.DEF, 449.82);
         this.setProperty(BattleProperties.SPD, 119);
         this.setProperty(BattleProperties.CRI, 1.5);
         this.setProperty(BattleProperties.CRI_DMG, 0.05);
         this.no = 304;
         this.name = "御馔津";
-        this.hp = 3001.6;
+        this.hp = 12646.12;
         this.addSkill(skill1);
         this.addSkill(skill2);
         this.addSkill(skill3);

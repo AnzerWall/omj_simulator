8a5b3b3b7ef9b6d2bb7d1e15fcd272235b1fb1d8
anzerwall <anzerwall@gmail.com>
2020-03-18 21:01:30 +0800

    feat: 完成简易调试界面

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import App from './App.vue';
 import 'ant-design-vue/dist/antd.css';
 import router from './router';
 import store from './store';
+import JsonTree from 'vue-json-tree'
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
+Vue.component('json-tree', JsonTree);
 
 new Vue({
   router,
--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -1,50 +1,151 @@
 <template>
     <div class="debug" ref="container">
-
+        <a-button @click="step" :block="false" style="margin-bottom: 20px;" >下一步</a-button>
+        <a-table :dataSource="microTasks" style="margin-bottom: 20px;" size="small">
+            <a-table-column title="提示" dataIndex="hint" />
+            <a-table-column title="数据" dataIndex="data" >
+                <template slot-scope="data">
+                    <json-tree :raw="data" :level="1"></json-tree>
+                </template>
+            </a-table-column>
+        </a-table>
+        <a-table :dataSource="tasks" style="margin-bottom: 20px;" size="small">
+            <a-table-column title="提示" dataIndex="hint" />
+            <a-table-column title="数据" dataIndex="data" >
+                <template slot-scope="data">
+                    <json-tree :raw="data" :level="1"></json-tree>
+                </template>
+            </a-table-column>
+        </a-table>
+        <a-table :dataSource="team1" style="margin-bottom: 20px;" rowKey="no" :pagination="false" size="small">
+            <a-table-column title="编号" dataIndex="no" />
+            <a-table-column title="名称" dataIndex="name" />
+            <a-table-column title="生命" dataIndex="hp" />
+            <a-table-column title="最大生命" dataIndex="max_hp" />
+            <a-table-column title="攻击" dataIndex="atk"  />
+            <a-table-column title="防御" dataIndex="def" />
+            <a-table-column title="速度" dataIndex="spd" />
+            <a-table-column title="暴击" dataIndex="cri" />
+            <a-table-column title="暴击伤害" dataIndex="cri_dmg" />
+            <a-table-column title="效果命中" dataIndex="eft_hit" />
+            <a-table-column title="效果抵抗" dataIndex="eft_res" />
+            <a-table-column title="伤害增加" dataIndex="dmg_dealt_buff" />
+            <a-table-column title="伤害降低" dataIndex="dmg_dealt_debuff" />
+            <a-table-column title="易伤" dataIndex="dmg_taken_buff" />
+            <a-table-column title="减伤" dataIndex="dmg_taken_debuff" />
+            <a-table-column title="忽略防御" dataIndex="def_neg" />
+            <a-table-column title="忽略防御百分比" dataIndex="def_neg_p" />
+            <a-table-column title="生命偷取" dataIndex="hp_steal" />
+        </a-table>
+        <a-table :dataSource="team2" rowKey="no"  :pagination="false" size="small">
+            <a-table-column title="编号" dataIndex="no" />
+            <a-table-column title="名称" dataIndex="name" />
+            <a-table-column title="生命" dataIndex="hp" />
+            <a-table-column title="最大生命" dataIndex="max_hp" />
+            <a-table-column title="攻击" dataIndex="atk"  />
+            <a-table-column title="防御" dataIndex="def" />
+            <a-table-column title="速度" dataIndex="spd" />
+            <a-table-column title="暴击" dataIndex="cri" />
+            <a-table-column title="暴击伤害" dataIndex="cri_dmg" />
+            <a-table-column title="效果命中" dataIndex="eft_hit" />
+            <a-table-column title="效果抵抗" dataIndex="eft_res" />
+            <a-table-column title="伤害增加" dataIndex="dmg_dealt_buff" />
+            <a-table-column title="伤害降低" dataIndex="dmg_dealt_debuff" />
+            <a-table-column title="易伤" dataIndex="dmg_taken_buff" />
+            <a-table-column title="减伤" dataIndex="dmg_taken_debuff" />
+            <a-table-column title="忽略防御" dataIndex="def_neg" />
+            <a-table-column title="忽略防御百分比" dataIndex="def_neg_p" />
+            <a-table-column title="生命偷取" dataIndex="hp_steal" />
+        </a-table>
     </div>
 </template>
 <script>
     import * as PIXI from 'pixi.js';
+    import Game from '../../core/system/game'
+    import datas from '../../core/fixtures/heros.json';
+    import {sampleSize, map, filter, forEach, values} from 'lodash';
+    import {BattleProperties} from "../../core/fixtures/hero-property-names"
 
-    export default {
-        mounted() {
-            const app = this.app = new PIXI.Application({
-                width: 1000,
-                height: 400,
-            });
-            this.$refs.container.appendChild(app.view);
-            app.loader.add('avator/10.png').load((loader, resources) => {
-                const avator = resources['avator/10.png'];
+    const ids = filter(map(datas, 'id'), d => !!d);
+    const entities = [];
+    [0, 1].forEach(teamId => {
 
-                const bunny = new PIXI.Sprite(avator.texture);
-                // Setup the position of the bunny
-                bunny.x = app.renderer.width / 2;
-                bunny.y = 100;
+        const keys = sampleSize(ids, 5); // 为每个队伍分配5个队员
 
-                // Rotate around the center
-                bunny.anchor.x = 0.5;
-                bunny.anchor.y = 0.5;
-
-                // Add the bunny to the scene we are building
-                app.stage.addChild(bunny);
-
-                // Listen for frame updates
-                app.ticker.add(() => {
-                    // each frame we spin the bunny around a bit
-                    bunny.x = bunny.x + Math.random() * 10 - 5;
-                    bunny.x = Math.max( 0 , bunny.x);
-                    bunny.x = Math.min( 500 , bunny.x)
-
-
-                });
+        for (const id of keys) {
+            entities.push({
+                no: id,
+                // eslint-disable-next-line @typescript-eslint/camelcase
+                team_id: teamId,
             });
         }
+        entities[0].no = 304;
+    });
+
+
+    const game = new Game(entities);
+
+    export default {
+        data() {
+            return {
+                team1: [],
+                team2: [],
+                microTasks: [],
+                tasks: [],
+            }
+        },
+        mounted() {
+          this.syncData();
+        },
+        methods: {
+            syncData() {
+                this.team1 = [];
+                this.team2 = [];
+                this.tasks = [];
+                this.microTasks = [];
+
+                game.entities.forEach(entity => {
+                    const data = {
+                        no: entity.no,
+                        name: entity.name,
+                        hp: entity.hp.toFixed(2),
+                    }
+                    forEach(values(BattleProperties), key => {
+                        data[key] = `${entity.getComputedProperty(key)}`
+                    });
+                   if (entity.team_id === 0) {
+                       this.team1.push(data)
+                   }
+                    if (entity.team_id === 1) {
+                        this.team2.push(data)
+                    }
+                });
+                game.microTasks.forEach(task => {
+                    this.microTasks.push({
+                        hint: task[2],
+                        data: JSON.stringify(task[1]),
+                    })
+                });
+                game.tasks.forEach(task => {
+                    this.tasks.push({
+                        hint: task[2],
+                        data: JSON.stringify(task[1]),
+                    })
+                });
+            },
+            step() {
+                game.process();
+                this.syncData();
+            }
+        }
+
     }
 </script>
 <style>
     .debug {
         display: flex;
         justify-content: center;
-        padding-top: 50px;
+        flex-direction: column;
+        padding: 20px;
     }
 </style>

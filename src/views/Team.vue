8be6cac360e976b3eb634d65325dc8ead7c907dd
anzer <anzerwall@gmail.com>
2020-03-24 01:24:42 +0800

    feat: 队伍设置支持级联选择，（碎觉了孩子

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,22 +3,18 @@
         <div style="font-weight: bold; margin-bottom: 20px">队伍1：</div>
         <div v-for="index in 5" :key="'t0_'+ index">
             式神{{index}}:
-            <a-select :defaultValue="team0[index - 1].no" :value="team0[index- 1].no" style="width: 120px"
-                      @change="handleChange(0, index-1, $event)">
-                <a-select-option v-for="hero in heroList" :value="hero.no" :key="hero.no">{{hero.name}}
-                </a-select-option>
-            </a-select>
+            <a-cascader  :value="convert(team0[index- 1].no)" style="width: 120px"  expandTrigger="hover" :allowClear="false"
+                      @change="handleChange(0, index-1, $event)" :options="heroList">
+            </a-cascader>
         </div>
         <a-divider/>
         <div style="font-weight: bold; margin-bottom: 20px">队伍2：</div>
 
         <div v-for="index in 5" :key="'t1_'+ index">
             式神{{index}}:
-            <a-select :defaultValue="team1[index - 1].no" :value="team1[index- 1].no" style="width: 120px"
-                      @change="handleChange(1, index-1, $event)">
-                <a-select-option v-for="hero in heroList" :value="hero.no" :key="hero.no">{{hero.name}}
-                </a-select-option>
-            </a-select>
+            <a-cascader :value="convert(team1[index- 1].no)" style="width: 120px"  expandTrigger="hover" :allowClear="false"
+                      @change="handleChange(1, index-1, $event)" :options="heroList">
+            </a-cascader>
         </div>
     </div>
 </template>
@@ -29,20 +25,55 @@
     export default {
         data() {
             return {
-                heroList: HeroData.map(hero => {
-                    return {
-                        no: hero.no,
-                        name: hero.name,
-                    };
-                }),
+                heroList: HeroData.reduce((list, hero) => {
+                    const p = list.find(d => d.value.toUpperCase() === hero.rank.toUpperCase());
+                    if (p) {
+                        p.children.push({
+                            value: hero.no,
+                            label: hero.name,
+                            children: [],
+                        });
+                    }
+                    return list;
+                }, [{
+                    value: 'sp',
+                    label: 'sp',
+                    children: [],
+                },{
+                    value: 'ssr',
+                    label: 'ssr',
+                    children: [],
+                },{
+                    value: 'sr',
+                    label: 'sr',
+                    children: [],
+                },{
+                    value: 'r',
+                    label: 'r',
+                    children: [],
+                },{
+                    value: 'n',
+                    label: 'n',
+                    children: [],
+                }]),
             }
         },
         computed: {
             ...mapState(['team0', "team1"])
         },
         methods: {
-            handleChange(teamId, index, no) {
-                this.$store.commit('UPDATE_TEAM_MEMBER', {teamId, index, no});
+            handleChange(teamId, index, seletions) {
+                this.$store.commit('UPDATE_TEAM_MEMBER', {teamId, index, no: seletions[1]});
+            },
+            convert(no) {
+                for(const o of this.heroList) {
+                    for(const c of o.children) {
+                        if (c.value === no) {
+                            return [o.value ,c.value];
+                        }
+                    }
+                }
+                return [];
             }
         }
     }
